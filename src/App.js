654f7aaf92a22bdbdf840d69b4774b653b8318d1
Justin Delille <justind7513@gmail.com>
2022-07-05 23:01:55 -0700

    added react routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './pages/dashboard/Dashboard';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Create from './pages/create/Create';
+import Project from './pages/project/Project';
+
 import './styles/App.scss';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<div className='container'>
+					<Switch>
+						<Route exact path='/'>
+							<Dashboard />
+						</Route>
+						<Route path='/create'>
+							<Create />
+						</Route>
+						<Route path='/project/:id'>
+							<Project />
+						</Route>
+						<Route path='/login'>
+							<Login />
+						</Route>
+						<Route path='/signup'>
+							<Signup />
+						</Route>
+					</Switch>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/project/Project.js
@@ -0,0 +1,5 @@
+import './Project.scss';
+
+export default function Project() {
+	return <div>Project</div>;
+}
